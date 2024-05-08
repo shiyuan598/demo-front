747c52db56904d344a17a738795253fe0bd03fac
wangshiyuan <wangshiyuan@zhito.com>
2024-05-08 16:02:06 +0800

    style

--- src/views/detail.tsx
+++ src/views/detail.tsx
@@ -1,28 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { releaseApi } from "../api";
 
-export default function Detail(props: {versions:{name:string;path:string}[]}) {
-    const {versions} = props;
+export default function Detail(props: { versions: { name: string; path: string }[] }) {
+    const { versions } = props;
     const { version: current } = useParams<{ version: string }>();
-    const [comment, setComment] = useState();
+    const contentRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         if (!current || !versions.length) {
             return;
         }
-        const path = versions.find(item => item.name === current)?.path;
-        path && releaseApi.getVersionDetail(path).then((v) => {
-            console.info(v);
-            v && setComment(v.data);
-        });
-
+        const path = versions.find((item) => item.name === current)?.path;
+        path &&
+            releaseApi.getVersionDetail(path).then((v) => {
+                if (contentRef.current) {
+                    contentRef.current.innerHTML = v.data.content;
+                }
+            });
     }, [current, versions]);
 
     return (
         <>
-            <div>detail</div>
-            <p>{JSON.stringify(comment)}</p>
+            <div ref={contentRef} className="content"></div>
         </>
     );
 }
