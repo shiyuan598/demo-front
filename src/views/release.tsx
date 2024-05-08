a1fadea2545be23e8578cba8edfe253c1a16b3e2
unknown <wangshiyuan@ZHITO.com>
2024-05-08 16:32:19 +0800

    'style'

--- src/views/release.tsx
+++ src/views/release.tsx
@@ -40,7 +40,6 @@ export default function Release() {
     return (
         <div className="release-page">
             <div className="aside">
-                <h6 className="title">Versions</h6>
                 <ul className="versions">
                     {versions.map((item) => (
                         <li
